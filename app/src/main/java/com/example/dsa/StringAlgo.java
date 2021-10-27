ce319f9b6238d10a4781ed0a7533b03b61e0c7d5
MPB <mp4learning@gmail.com>
2021-10-27 12:36:11 +0530

    Check if string is Pangram

---
+++ app/src/main/java/com/example/dsa/StringAlgo.java
@@ -0,0 +1,74 @@
+package com.example.dsa;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class StringAlgo {
+
+    public void pangramCheck() {
+        String str = "The quick brown fox jumps over the lazy dog";
+        int index = 0;
+        boolean[] mark = new boolean[26];
+        boolean isPangram = false;
+
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                index = str.charAt(i) - 'a';
+            } else if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+                index = str.charAt(i) - 'A';
+            } else {
+                continue;
+            }
+            mark[index] = true;
+        }
+
+        for (int x = 0; x < 26; x++) {
+            isPangram = mark[x] != false;
+        }
+
+        if (isPangram) {
+            Log.v("StringAlgo", "Given string is Pangram");
+        } else {
+            Log.v("StringAlgo", "Given string is not Pangram");
+        }
+    }
+
+    public void missingCharFromPangram() {
+        String str = "The quick brown fox jumps over the lazy dog";
+
+        boolean[] mark = new boolean[26];
+        int index = 0;
+
+        for (int x = 0; x < 26; x++) {
+            Log.v("StringAlgo", "Mark:"+mark[x]);
+        }
+        ArrayList<Character> MissingChar = new ArrayList<>(26);
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                index = str.charAt(i) - 'a';
+            } else if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+                index = str.charAt(i) - 'A';
+            } else {
+                continue;
+            }
+            mark[index] = true;
+        }
+        for (int x = 0; x < 26; x++) {
+            if(mark[x] == false) {
+                MissingChar.add((char)(x + 'a'));
+            }
+        }
+
+        for (int x = 0; x < MissingChar.size(); x++) {
+            Log.v("StringAlgo", "Missing Char:"+MissingChar.get(x));
+        }
+
+    }
+
+}
+
