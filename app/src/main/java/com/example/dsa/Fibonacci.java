89e28427e543cef892b5fecf98a0a1aad91c8b8b
MPB <mp4learning@gmail.com>
2021-11-20 15:53:11 +0530

    Array Rotate.

--- app/src/main/java/com/example/dsa/Fibonacci.java
+++ app/src/main/java/com/example/dsa/Fibonacci.java
@@ -53,10 +53,10 @@ public class Fibonacci {
     }
 
     void maximumOfAllSubArray() {
-        int k =3;
+        int k = 3;
         int sum_so_far = 0;
-        int arr[] = { 1, 4, 3 ,1 ,0, 5 ,2, 3 ,6,8};
-        for ( int i = 0; i <=arr.length - k; i++ ) {
+        int arr[] = {1, 4, 3, 1, 0, 5, 2, 3, 6, 8};
+        for ( int i = 0; i <= arr.length - k; i++ ) {
 
             int max = 0;
 
@@ -76,29 +76,77 @@ public class Fibonacci {
         }
     }
 
-//    Given an array of integers and a number x, find the smallest subarray with sum greater than the given value.
-    void smallestSubArray() {
 
-        int arr[] = {1, 4, 45, 6, 0, 19};
-        int x = 51;
+    void sumofAllSubarrays() {
+        int a[] = {1, 2, 3};
 
-        int min_length = arr.length+1;
+        int sum = 0;
 
-        for(int i=0;i<arr.length;i++) {
-            int sum_so_far = arr[i];
-
-            if(sum_so_far>x) {
-                return;
-            }
-            for(int j=i+1;j<arr.length;j++) {
-                sum_so_far = sum_so_far+ arr[j];
-
-                if(sum_so_far > x && (min_length > (j-i+1))) {
-                   min_length = (j-i+1);
-                }
-
+        for ( int i = 0; i < a.length; i++ ) {
+            int temp = 0;
+            for ( int j = i; j < a.length; j++ ) {
+                temp = temp + a[j];
+                sum = sum + temp;
             }
         }
-        Log.v(TAG,"Min length:"+min_length);
+
+        Log.v(TAG, "Sum of all SubArrays:" + sum);
+    }
+
+    void subArrayWithGivenSum() {
+        int a[] = {1, 4, 20, 3, 10, 5};
+        int sum = 33;
+
+        for ( int i = 0; i < a.length; i++ ) {
+
+            int temp = 0;
+
+            for ( int j = i; j < a.length; j++ ) {
+
+                if (temp == sum) {
+                    Log.v(TAG, "SubArray with given sum can be found b/w: " + i + " And " + j);
+                    return;
+                }
+                temp = temp + a[j];
+            }
+        }
+    }
+
+    //    Given an array of integers and a number x, find the smallest subarray with sum greater than the given value.
+    void smallestSubArrayWithSumGreater() {
+        int a[] = {1, 11, 100, 1, 0, 200, 3, 2, 1, 250};
+        int sum = 280;
+        int smallest_so_far = a.length;
+        for ( int i = 0; i < a.length; i++ ) {
+            int temp_sum = 0;
+            for ( int j = i; j < a.length; j++ ) {
+                temp_sum = temp_sum + a[j];
+
+                if (temp_sum > sum && smallest_so_far > (j - i + 1)) {
+                    smallest_so_far = (j - i + 1);
+                }
+            }
+        }
+        Log.v(TAG, "Smallest SubArray with :" + smallest_so_far);
+    }
+
+    void rotateLeftArray() {
+        int a[] =  {1, 2, 3, 4, 5, 6, 7};
+        int d = 2;
+
+
+        for(int m=0;m<d;m++) {
+            int temp = a[0];
+
+            for(int i=0;i<a.length-1;i++) {
+                a[i] = a[i+1];
+            }
+            a[a.length-1] = temp;
+        }
+
+        for(int j=0;j<a.length;j++) {
+            Log.v(TAG,"Rotated Array is:"+a[j]);
+        }
+
     }
 }
